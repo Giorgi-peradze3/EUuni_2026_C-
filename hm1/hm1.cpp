ca8f03f5c68f1b4cbbec394bde7bbff48aa190b8
gioo333 <giorgiperadze33@gmail.com>
2026-03-12 19:58:51 +0400

    first homework

---
+++ hm1/hm1.cpp
@@ -0,0 +1,136 @@
+#include <iostream>
+using namespace std;
+
+
+// task 1
+//int main(){
+//	
+//	int income = 0;
+//	int tax = 0;
+//	int hand_take = 0;
+//	
+//	cout << "shemoitanet shemosavali: ";
+//	cin >> income;
+//	
+//	if (income > 1000){
+//		tax = income * 0.20;
+//		
+//	}else{
+//		tax = 0;
+//	}
+//	
+//	hand_take = income - tax;
+//	
+//	
+//	cout << "shesatani tanxa: " << tax << " lari" << endl;
+//    cout << "xelze asagebi tanxa: " << hand_take << " lari" << endl;
+//
+//    return 0;
+//	
+//	
+//}
+
+
+// task 2
+//int main(){
+//	
+//	int all = 0;
+//	int been = 0;
+//	int total = 0;
+//	
+//	cout << "gtxovt sheiyvanot leqciebis sruli raodenoba: ";
+//	cin >> all;
+//	cout << "gtxovt sheiyvanet ramdenze iyavit: ";
+//	cin >> been;
+//	
+//	if (been < all * 0.75){
+//		cout << "tqven ver daishvebit gamocdaze gacdenebis gadacharbebis gamo";
+//	} else{
+//		cout << "miibrzandit gamocdaze";
+//	}
+//	
+//	
+//	
+//	cout << "gamocdaze dasashvebat sachiroa" << all * 0.75 << endl;
+//	cout << "tqven gaqvt" << been << endl;
+//	
+//	return 0;
+//	
+//}
+
+
+// task 3
+//int main(){
+//	
+//	int first = 0;
+//	int second = 0;
+//	int third = 0;
+//	int fourth = 0;
+//	int fifth = 0;
+//	int sixth = 0;
+//	int total = 0;
+//	
+//	cout << "gtxovt sheiyvanot pirveli qula: ";
+//	cin >> first;
+//	cout << "gtxovt sheiyvanot meore qula: ";
+//	cin >> second;
+//	cout << "gtxovt sheiyvanot mesame qula: ";
+//	cin >> third;
+//	cout << "gtxovt sheiyvanot meotxe qula: ";
+//	cin >> fourth;
+//	cout << "gtxovt sheiyvanot mexute qula: ";
+//	cin >> fifth;
+//	cout << "gtxovt sheiyvanot meeqvse qula: ";
+//	cin >> sixth;
+//	
+//	total = (first + second + third + fourth + fifth + sixth) / 6;
+//	
+//	
+//	if (total >= 91 && total <= 100) {
+//    	cout << "tqveni qulaa A";
+//	} else if (total >= 81 && total <= 90) {
+//	    cout << "tqveni qulaa B";
+//	} else if (total >= 71 && total <= 80) {
+//	    cout << "tqveni qulaa C";
+//	} else if (total >= 61 && total <= 70) {
+//	    cout << "tqveni qulaa D";
+//	} else if (total >= 51 && total <= 60) {
+//	    cout << "tqveni qulaa E";
+//	} else {
+//	    cout << "tqveni qulaa F, tqven ver gaiaret sagani";
+//	}
+//	
+//	return 0;
+//		
+//}
+
+
+// task 4
+//int main(){
+//	
+//	int a, b, c;
+//	
+//    cout << "shemoiyvanet sami ricxvi: ";
+//    cin >> a >> b >> c;
+//
+//	
+//	if (a >= b && b >= c) {
+//        cout << "1: " << a << "n2: " << b << "n3: " << c << endl;
+//    }else if (a >= c && c >= b) {
+//        cout << "1: " << a << "n2: " << c << "n3: " << b << endl;
+//    }else if (b >= a && a >= c) {
+//        cout << "1: " << b << "n2: " << a << "n3: " << c << endl;
+//    }else if (b >= c && c >= a) {
+//        cout << "1: " << b << "n2: " << c << "n3: " << a << endl;
+//    } else if (c >= a && a >= b) {
+//        cout << "1: " << c << "n2: " << a << "n3: " << b << endl;
+//    } else if (c >= b && b >= a) {
+//        cout << "1: " << c << "n2: " << b << "n3: " << a << endl;
+//    }
+//    
+//    
+//	return 0;
+//}
+
+
+
