448541dec1377490fd062da7a6df06c400add9ba
gioo333 <giorgiperadze33@gmail.com>
2026-03-19 19:40:19 +0400

    second homework

---
+++ hm2/hm2.cpp
@@ -0,0 +1,140 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// task 1
+//int main(){
+//	char a;
+//	cin >> a;
+//	
+//	while (a != '!'){
+//		if (a != 'a' && a != 'e' && a != 'i' && a != 'o' && a != 'u'){
+//			cout << a << "araris xmovani" << endl;
+//		}else{
+//			cout << a << "aris xmovani" << endl;
+//		}
+//		cin >> a;
+//		
+//	}
+//	
+//	return 0;
+//}
+
+// task 2 
+//int main(){
+//	int i;
+//	
+//	for(i = 1; i < 1000; i++){
+//		int result = i;
+//		
+//		int j;
+//		for(j = 1; j < i; j++){
+//			if(j % i == 0){
+//				result += j;
+//			}
+//			cout << result << endl;
+//		}
+//		
+//	}
+//	
+//	
+//	return 0;
+//}
+
+//task 3
+//
+//int main(){
+//	
+//	int price[20];
+//	int all = 0;
+//	
+//	
+//	for(int i = 0; i < 20; i++){
+//		cin >> price[i];
+//		all += price[i];
+//		
+//	}
+//	
+//	for(int i = 0; i < 20; i++){
+//		if(price[i] < 5){
+//			cout << price[i] << endl;
+//		}
+//	}
+//	
+//	double mid = all / 20;
+//	cout << "sashualo: " << mid <<  endl;
+//	
+//	for(int i = 0; i < 20; i++){
+//		if(price[i] > mid){
+//			cout << price[i] << endl;
+//		}
+//	}
+//	
+//	return 0;
+//	
+//}
+
+//task 4 ??
+//int main(){
+//	
+//	int a;
+//	cout << "gtxovt sheiyvanot sagnebis raodenoba: " << endl;
+//	cin >> a;
+//	
+//	
+//	int grade;
+//	for(int i = 0; i <= a; i++){
+//		cout << i << "-e sagnis qula: " << endl;
+//		cin >> grade;
+//		
+//		
+//	}
+//	
+//	return 0;
+//	
+//}
+
+//task 5
+//
+//int main(){
+//	
+//	string a;
+//	cin >> a;
+//	
+//	if (a == a[::-1]){
+//		cout << a << "is polydrome";
+//	}else{
+//		cout << a << "is not polydrome";
+//	}
+//	
+//	return 0;
+//	
+//}
+
+//task 6 ??
+
+//int main(){
+//	int a;
+//	cout << "gtxovt sheiyvanot ricxvebis raodenoba: " << endl;
+//	cin >> a;
+//	
+//	int number;
+//	for(int i = 0; i <= a; i++){
+//		cout << i << "-e sagnis qula: " << endl;
+//		cin >> number;
+//		
+//		
+//	}	
+//	
+//	return 0;
+//	
+//}
+
+
+
+
+
+
+
+
+
