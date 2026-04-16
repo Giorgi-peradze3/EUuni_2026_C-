55e3f72e78e9e446cb288e2cfb30c2611ad97e00
gioo333 <giorgiperadze33@gmail.com>
2026-04-16 20:52:07 +0400

    quiz1

---
+++ quiz1/quiz1.cpp
@@ -0,0 +1,159 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+// task 1
+
+//int main(){
+//	
+//	long long l = 100000000;
+//
+//  		cout << sizeof(l) << endl;
+//	
+//}
+
+// result: 8
+
+//task 2
+
+//int main(){
+//	
+//	cout << sizeof(double) << endl;
+//	
+//}
+
+// result: 8
+
+
+// task 3
+
+//int main(){
+//	
+//  int i = 0;
+//
+//    while(i < n) {
+//
+//        if (arr[i] == x) {
+//
+//            cout << true << endl;
+//
+//            return 0;
+//
+//        }
+//        i++;
+//
+//    }
+//
+//    cout << false;
+//
+//    return 0;
+//	
+//	
+//	
+//}
+
+// task 4
+//
+//int main(){
+//	
+//	int a;
+//	cout << "please enter ur age: " << endl;
+//	cin >> a;
+//	
+//	if(a <= 18){
+//		cout << "price for one week: 25$" << endl;
+//		cout << "price for whole course: " << 25*15 << endl;
+//	}if(18 < a & a <= 24){
+//		cout << "price for one week: 35$" << endl;
+//		cout << "price for whole course: " << 35*15 << endl;
+//	}if(a >= 25){
+//		cout << "price for one week: 50$" << endl;
+//		cout << "price for whole course: " << 50*15 << endl;
+//	}
+//	
+//	
+//	return 0;
+//	
+//}
+
+// task 5
+//
+//int main(){
+//	
+//	int a;
+//	int b;
+//	cout << "please enter number: " << endl;
+//	cin >> a;
+//	
+//	while(a > 0){
+//		
+//		b += a % 10;
+//		a /= 10;	
+//	}
+//	
+//	if(b % 3 == 0){
+//			cout << "ur number can be devided by 3" << endl;
+//		}else{
+//			cout << "ur number can't be devided by 3" << endl;
+//	}
+//	
+//	return 0;
+//	
+//	
+//}
+
+// task 6
+//
+//int main() {
+//    int a[] = {1, 3, 5, 6};
+//    int size = 4;
+//    int b; 
+//    cout << "please enter number: ";
+//    cin >> b;
+//
+//    for (int i = 0; i < size; i++) {
+//       
+//        if (a[i] >= b) {
+//            cout << i << endl;
+//            return 0;
+//        }
+//    }
+//    
+//    cout << size << endl;
+//
+//    return 0;
+//}
+
+//task 7
+//
+//int main(){
+//	
+//	int a;
+//	cout << "enter number: ";
+//	cin >> a;
+//	string sum = "";
+//	
+//	
+//	int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+//    string roman[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+//    
+//    for(int i = 0; i < 13; i++){
+//    	while(a >= values[i]){
+//    		sum += roman[i];
+//    		a -= values[i];
+//		}
+//	}
+//	
+//	cout << "roman: " << sum << endl;
+//	return 0;
+//	
+//}
+
+
+
+
+
+
+
+
+
