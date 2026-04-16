8e546f59a875c5e1e6fc44f293723ce90bce3103
gioo333 <giorgiperadze33@gmail.com>
2026-04-16 20:49:20 +0400

    hm3

---
+++ hm3/hm3.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+using namespace std;
+
+// task 2
+//int main(){
+//	
+//	int price;
+//	cout << "please enter price: ";
+//	cin >> price;
+//	
+//	cout << "with tax: " <<  price % 18 << "tax: 18%" << endl;
+//	
+//}
+
+// task 3
+//
+//int main(){
+//	
+//	int a;
+//	cout << "please enter second: ";
+//	cin >> a;
+//	
+//	int hours = a / 3600; 
+//    
+//   
+//    int mins = (a % 3600) / 60;
+//    
+//    
+//    int secs = a % 60;
+//	
+//	
+//	cout << "Hours: " << hours << " | Mins: " << mins << " | Secs: " << secs << endl;
+//	
+//	
+//}
+
+// task 4 must ask ??
